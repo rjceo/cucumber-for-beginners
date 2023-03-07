b8d5b71ba160988ff95c05872bf41f374d7f1ff9
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-03-07 17:10:55 +0800

    Scenario - successful login

--- src/test/java/com/cucumberpractice/StepDefinitions.java
+++ src/test/java/com/cucumberpractice/StepDefinitions.java
@@ -1,30 +1,74 @@
 package com.cucumberpractice;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
 public class StepDefinitions {
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
-        System.setProperty("webdriver.chrome.com", "src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver;
 
+    @Before
+    public void beforeScenario() {
+        System.setProperty("webdriver.chrome.com", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
-
+    @After
+    public void afterScenario() {
+        //driver.close();
+        driver.quit();
     }
 
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
-        Assert.fail();
+    @Given("^login page \"(.*?)\" is open$")
+    public void loginPage(String strURL) {
+        driver.get(strURL);
     }
 
+    @When("^user enters \"(.*?)\" as the Username$")
+    public void userEntersAsTheUsername(String strUsername) {
+        driver.findElement(By.id("username")).sendKeys(strUsername.trim());
+    }
+
+    @And("^user enters \"(.*?)\" as the Password$")
+    public void userEntersAsThePassword(String strPassword) {
+        driver.findElement(By.id("password")).sendKeys(strPassword.trim());
+    }
+
+    @And("clicks on the login button")
+    public void clicksOnTheLoginButton() throws InterruptedException {
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(2000);
+    }
+
+    @Then("^page is redirected to \"(.*?)\"$")
+    public void pageIsRedirectedTo(String strExpectedURL) {
+        String strActualURL = driver.getCurrentUrl().toLowerCase().trim();
+        if (strActualURL.compareTo(strExpectedURL.trim().toLowerCase()) != 0) {
+            Assert.fail(
+                    "Expected: " + strExpectedURL
+                            + "\n Actual: " + strActualURL
+            );
+        }
+    }
+
+    @And("^message \"(.*?)\" is displayed$")
+    public void messageIsDisplayed(String strExpectedMesg) {
+        String[] strActualMesg = driver.findElement(By.id("flash")).getText().trim().split("\n");
+
+        if (strActualMesg[0].compareTo(strExpectedMesg.trim()) != 0) {
+            Assert.fail(
+                    "Expected: " + strExpectedMesg
+                            + "\n Actual: " + strActualMesg[0]
+            );
+        }
+    }
 }
--- src/test/resources/com/cucumberpractice/Login Page.feature
+++ src/test/resources/com/cucumberpractice/Login Page.feature
@@ -1,19 +1,21 @@
 Feature: Login Page
 
+  @positive
   Scenario: Successful Login
-    Given login page is open
-    When user enters the user ID
-    And user enters the password
+    Given login page "https://the-internet.herokuapp.com/login" is open
+    When user enters "tomsmith" as the Username
+    And user enters "SuperSecretPassword!" as the Password
     And clicks on the login button
-    Then success message is displayed in the landing page
+    Then page is redirected to "https://the-internet.herokuapp.com/secure"
+    And message "You logged into a secure area!" is displayed
 
-  Scenario Outline: <sn> Outline Example
-    Given example
-    When <a>
-    And  <b>
-    Then ex
-
-    Examples:
-      | sn | a        | b        |
-      | 01 | asdfsf   | asdfasdf |
-      | 02 | asdfasdf | asdfas   |
+#  Scenario Outline: <sn> Outline Example
+#    Given example
+#    When <a>
+#    And  <b>
+#    Then ex
+#
+#    Examples:
+#      | sn | a        | b        |
+#      | 01 | asdfsf   | asdfasdf |
+#      | 02 | asdfasdf | asdfas   |
